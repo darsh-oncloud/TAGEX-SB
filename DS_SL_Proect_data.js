666af9b28e23275239f195daf1640b0c31f1ee47
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-05-08 12:35:31 -0400

    Update from NetSuite (Auto)

--- DS_SL_Proect_data.js
+++ DS_SL_Proect_data.js
@@ -1032,6 +1032,7 @@ var noteId = noteRec.save({
               <td class="wrap note-cell-wrap">
                 <div class="note-draft-list" data-note-box="${escHtml(row.recId)}"></div>
                 <button type="button" class="note-add-btn btn-add-note" data-recid="${escHtml(row.recId)}">+ Add Note</button>
+                <button type="button" class="note-add-btn btn-view-note" data-recid="${escHtml(row.recId)}" style="margin-left:6px;">View Notes</button>
               </td>
             </tr>
           `;
@@ -1628,12 +1629,13 @@ var noteId = noteRec.save({
     return 'tmp_' + new Date().getTime() + '_' + Math.floor(Math.random() * 100000);
   }
 
-  function openNotePopup(recId, tempId, title, memo){
+  function openNotePopup(recId, tempId, title, memo, mode){
     var popupUrl = NOTE_POPUP_URL
       + '&projectid=' + encodeURIComponent(recId || '')
       + '&tempid=' + encodeURIComponent(tempId || '')
       + '&title=' + encodeURIComponent(title || '')
-      + '&memo=' + encodeURIComponent(memo || '');
+      + '&memo=' + encodeURIComponent(memo || '')
+      + '&mode=' + encodeURIComponent(mode || 'add');
 
     console.log('Opening note popup', { recId: recId, tempId: tempId });
     window.open(popupUrl, 'project_note_popup_' + (recId || ''), 'width=720,height=560,resizable=yes,scrollbars=yes');
@@ -2258,8 +2260,13 @@ var noteId = noteRec.save({
     document.addEventListener('click', function(e){
       var addBtn = e.target.closest('.btn-add-note');
       if (addBtn) {
-        openNotePopup(addBtn.getAttribute('data-recid'), '', '', '');
+        openNotePopup(addBtn.getAttribute('data-recid'), '', '', '', 'add');
         return;
+      
+        var viewBtn = e.target.closest('.btn-view-note');
+       if (viewBtn) {
+       openNotePopup(viewBtn.getAttribute('data-recid'), '', '', '', 'view');
+      return;
       }
 
       var editBtn = e.target.closest('.btn-edit-note');
